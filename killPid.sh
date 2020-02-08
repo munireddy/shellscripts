b61e947fd515ab6096652532e6140de617a487a0
munireddy <munisekhar.reddy@gmail.com>
2020-02-07 21:05:49 -0500

    update

--- killPid.sh
+++ killPid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This scripts lists all the python processes running on your machine
-ps -ef | grep python | grep -v grep | cut -d" " -f1 -f2 -f3
+ps -ef | grep python | grep -v grep | kill $(cut -d" " -f4) 
