7c6ff67d23dc48643283cd14569d618cdfa98211
munireddy <munisekhar.reddy@gmail.com>
2020-02-15 08:24:18 -0500

    Excecises

---
+++ bashScriptExcecises/script1.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "ls > file_list"
+ls > file_list
+ 
+echo "ls -la >> file_list"
+ls -la >> file_list
+ 
+echo "cat < file_list"
+cat < file_list
---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Get the key for jenkins.
+#wget is used to download
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+#sh -c : run the string after -c as a command on the shell.
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
