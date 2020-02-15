daa524d9d1ff6562718700e8bc5ef8cefe16e489
munireddy <munisekhar.reddy@gmail.com>
2020-02-15 09:11:19 -0500

    Create script7.sh

---
+++ bashScriptExcecises/script7.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ $1 -gt 0 ]; then
+  echo "$1 is positive"
+elif [ $1 -lt 0 ]
+then
+  echo "$1 is negative"
+elif [ $1 -eq 0 ]
+then
+  echo "$1 is zero"
+else
+  echo "Opps! $1 is not number, give number"
+fi
