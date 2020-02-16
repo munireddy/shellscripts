e74ef0075eaa827d11c3c2914f76525da2e81524
munireddy <munisekhar.reddy@gmail.com>
2020-02-16 09:01:06 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,3 +6,4 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
+#Comments
